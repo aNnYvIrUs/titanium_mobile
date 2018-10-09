6ab361125de91402e5e62729744553c013df103f
Christopher Williams <chris.a.williams@gmail.com>
2018-10-09 14:54:29 -0400

    Migrate Ti.Gesture to new proxy style

--- iphone/Classes/GestureModule.h
+++ iphone/Classes/GestureModule.h
@@ -1,26 +1,35 @@
 /**
  * Appcelerator Titanium Mobile
- * Copyright (c) 2009-2010 by Appcelerator, Inc. All Rights Reserved.
+ * Copyright (c) 2009-Present by Appcelerator, Inc. All Rights Reserved.
  * Licensed under the terms of the Apache Public License
  * Please see the LICENSE included with this distribution for details.
  */
-#import <TitaniumKit/TiModule.h>
-
 #ifdef USE_TI_GESTURE
+#import <JavaScriptCore/JavaScriptCore.h>
+#import <TitaniumKit/ObjcProxy.h>
 
-@interface GestureModule : TiModule {
+@protocol GestureExports <JSExport>
+
+@property (readonly) BOOL landscape;
+@property (readonly) NSNumber *orientation;
+@property (readonly) BOOL portrait;
+
+// TODO These are deprecated, remove in SDK 9.0.0
+// accessors
+- (BOOL)getLandscape __attribute((deprecated("Use the property instead.")));
+- (NSNumber *)getOrientation __attribute((deprecated("Use the property instead.")));
+- (BOOL)getPortrait __attribute((deprecated("Use the property instead.")));
+
+// methods
+// TODO: Add isFaceDown() and isFaceUp()?
+// - (BOOL)isFaceDown;
+// - (BOOL)isFaceUp;
+
+@end
+
+@interface GestureModule : ObjcProxy <GestureExports> {
   NSTimeInterval lastShakeTime;
 }
-
-@property (nonatomic, readonly) NSNumber *orientation;
-
-@property (nonatomic, readonly) NSNumber *PORTRAIT;
-@property (nonatomic, readonly) NSNumber *LANDSCAPE_LEFT;
-@property (nonatomic, readonly) NSNumber *LANDSCAPE_RIGHT;
-@property (nonatomic, readonly) NSNumber *UPSIDE_PORTRAIT;
-@property (nonatomic, readonly) NSNumber *UNKNOWN;
-@property (nonatomic, readonly) NSNumber *FACE_UP;
-@property (nonatomic, readonly) NSNumber *FACE_DOWN;
 
 @end
 
